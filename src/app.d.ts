1336fa377a52e70c300fdf0a5468ccbb2981b242
binhth <binhth@hocmai.vn>
2022-09-05 18:02:16 +0700

    feat: init project

---
+++ src/app.d.ts
@@ -0,0 +1,13 @@
+// See https://kit.svelte.dev/docs/types#app
+// for information about these interfaces
+// and what to do when importing types
+declare namespace App {
+	// eslint-disable-next-line @typescript-eslint/naming-convention
+	interface Locals {
+		userid: string
+	}
+
+	// interface PageData {}
+
+	// interface Platform {}
+}
