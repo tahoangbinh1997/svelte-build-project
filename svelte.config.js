b84aa88668e87db7577e2104f187940651b67473
binhth <binhth@hocmai.vn>
2022-09-13 16:11:53 +0700

    fix: crs loading

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto'
+import adapter from '@sveltejs/adapter-static'
 import preprocess from 'svelte-preprocess'
 import { readFile } from 'fs/promises'
 import { searchForWorkspaceRoot } from 'vite'
@@ -10,13 +10,22 @@ const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
+	extensions: ['.svelte', '.md'],
 
 	kit: {
 		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
-			fallback: 'app.html'
-		}) // edge fix
+			fallback: null,
+			precompress: false
+		})
+	},
+
+	prerender: {
+		default: true,
+		entries: ['*']
 	},
 
 	// hydrate the <div id="svelte"> element in src/app.html
